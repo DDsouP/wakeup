c17679ba3044e1ee3bcc9bbc4d6d1c340e957e62
DDsouP <101051097+DDsouP@users.noreply.github.com>
2023-06-11 21:34:21 +0800

    Update VideoSeeActivity.kt

--- app/src/main/java/com/example/myapplication/VideoSeeActivity.kt
+++ app/src/main/java/com/example/myapplication/VideoSeeActivity.kt
@@ -16,7 +16,7 @@ class VideoSeeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_see)
-
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         intent = getIntent()
         if(intent.getData()!=null) {
             uri = intent.getData()!!
